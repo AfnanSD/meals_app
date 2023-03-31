4bcf0f5b8d522d700aa467583d53d53dc12c9d56
AfnanSD <99503463+AfnanSD@users.noreply.github.com>
2023-03-31 03:36:42 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -73,6 +73,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.teal,
